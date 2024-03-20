0d55c6ee8711288e153fdd081cdddc19dd03ae8d
Bence Gombkötő <kucukasd@gmail.com>
2024-03-20 20:20:57 +0100

    collide-check

--- src/main.ts
+++ src/main.ts
@@ -6,21 +6,21 @@ let moveUp: { [key: string]: boolean } = {};
   const app = new PIXI.Application();
   await app.init({ antialias: true, width: 700, height: 500 });
   document.body.appendChild(app.canvas);
-  console.log(app);
 
   const player = new PIXI.Graphics().circle(0, 0, 30).fill("green");
   player.x = 200;
-  player.y = 200;
+  player.y = app.canvas.height / 2;
+  player.pivot.set(0.5);
   app.stage.addChild(player);
+
+  const ceil = new PIXI.Graphics().rect(0, 0, app.canvas.width, 20).fill("red");
+  app.stage.addChild(ceil);
 
   const floor = new PIXI.Graphics()
     .rect(0, 0, app.canvas.width, 20)
     .fill("red");
   floor.y = app.canvas.height - floor.height;
   app.stage.addChild(floor);
-
-  const ceil = new PIXI.Graphics().rect(0, 0, app.canvas.width, 20).fill("red");
-  app.stage.addChild(ceil);
 
   window.addEventListener("keydown", keyDown);
   window.addEventListener("keyup", keyUp);
@@ -33,13 +33,33 @@ let moveUp: { [key: string]: boolean } = {};
     moveUp[e.key] = false;
   }
 
-  function movePlayer(): void {
-    if (moveUp[" "]) {
-      player.y -= 5;
-    } else {
-      player.y += 5;
-    }
+  // function movePlayer(): void {
+  //   if (moveUp[" "]) {
+  //     player.y -= 5;
+  //   } else {
+  //     player.y += 5;
+  //   }
+  // }
+
+  function collide(player: PIXI.Graphics): boolean {
+    const playerTop = player.y - player.height / 2;
+    const playerBottom = player.y + player.height / 2;
+
+    const ceilBottom = ceil.y + ceil.height;
+    const floorTop = floor.y;
+
+    if (playerTop <= ceilBottom || playerBottom >= floorTop) {
+      return true;
+    } else return false;
   }
 
-  app.ticker.add(movePlayer);
+  app.ticker.add(() => {
+    if (!collide(player)) {
+      if (moveUp[" "]) {
+        player.y -= 5;
+      } else {
+        player.y += 5;
+      }
+    }
+  });
 })();
