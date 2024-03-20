1db1632eb71e1ce620193cd05f910fd733937ece
Bence Gombkötő <kucukasd@gmail.com>
2024-03-20 19:05:59 +0100

    pixi-flappy

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { Application, Graphics } from "pixi.js";
+
+(async () => {
+  const app = new Application();
+  await app.init({ antialias: true, width: 700, height: 500 });
+  document.body.appendChild(app.canvas);
+  console.log(app);
+
+  const player = new Graphics().circle(0, 0, 30).fill("red");
+  player.x = 200;
+  player.y = 200;
+  app.stage.addChild(player);
+
+  const floor = new Graphics().rect(0, 0, app.canvas.width, 20).fill("red");
+  floor.y = app.canvas.height - floor.height;
+  app.stage.addChild(floor);
+
+  const ceil = new Graphics().rect(0, 0, app.canvas.width, 20).fill("red");
+  app.stage.addChild(ceil);
+})();
