04812de41b487102070b33bdf624c67094ab7ea4
Bence Gombkötő <kucukasd@gmail.com>
2024-03-20 19:07:50 +0100

    pixi-import

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,22 @@
-import { Application, Graphics } from "pixi.js";
+import * as PIXI from "pixi.js";
 
 (async () => {
-  const app = new Application();
+  const app = new PIXI.Application();
   await app.init({ antialias: true, width: 700, height: 500 });
   document.body.appendChild(app.canvas);
   console.log(app);
 
-  const player = new Graphics().circle(0, 0, 30).fill("red");
+  const player = new PIXI.Graphics().circle(0, 0, 30).fill("green");
   player.x = 200;
   player.y = 200;
   app.stage.addChild(player);
 
-  const floor = new Graphics().rect(0, 0, app.canvas.width, 20).fill("red");
+  const floor = new PIXI.Graphics()
+    .rect(0, 0, app.canvas.width, 20)
+    .fill("red");
   floor.y = app.canvas.height - floor.height;
   app.stage.addChild(floor);
 
-  const ceil = new Graphics().rect(0, 0, app.canvas.width, 20).fill("red");
+  const ceil = new PIXI.Graphics().rect(0, 0, app.canvas.width, 20).fill("red");
   app.stage.addChild(ceil);
 })();
