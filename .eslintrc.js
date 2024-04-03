b32f19671539fe43440632cf128b306492aa6824
Bence Gombkötő <kucukasd@gmail.com>
2024-04-03 20:32:19 +0200

    eslintrc other config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,13 +4,12 @@ module.exports = {
     es6: true,
     node: true,
   },
+  parserOptions: {
+    ecmaVersion: 8,
+    sourceType: "module",
+  },
   extends: ["eslint:recommended", "google"],
   rules: {
     quotes: ["error", "double"],
-  },
-
-  // Newly added property
-  parserOptions: {
-    ecmaVersion: 2020,
   },
 };
